36fd3ccb84d67826a9bdbd0552431c317e5a9f7f
Fallfromgrace18 <149462981+Fallfromgrace18@users.noreply.github.com>
2024-05-17 12:13:23 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+// Write a function called `superbowlWin()` in `index.js`:
+
+// * The function should receive 1 argument, an `Array` of JavaScript `Object`s
+// * Each object has two properties: `year` and `result`
+// * It should use `find()` to test each `Object` to see if the `result` is `"W"` —
+//   a win!
+// * It should return the `year` when the win occurred (if it occurred at all!)
+// * If no win is found, it should return, sadly, `undefined`
+
+function superbowlWin(arrayOfRecords){
+ const found = arrayOfRecords.find((element)=>element.result==="W") 
+console.log("this is our variable found: ",found)
+if (found===undefined){
+    return(undefined)
+}else { return(found.year)}
+}
+
+
+    
+
+                                                                                                                                          
